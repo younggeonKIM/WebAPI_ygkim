05e0012a1214b5c2d6c78fd6b059845d619604ab
영건 김 <young3702@naver.com>
2024-02-06 21:15:03 +0900

    20240206_WEBAPIStudy(DbContext)_updated

---
+++ WebApplication1/WebApplication1/Models/TodoContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Models
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options) : base(options)
+        {
+
+        }
+        public DbSet<TodoItem> TodoItems { get; set; } = null;
+    }
+}
