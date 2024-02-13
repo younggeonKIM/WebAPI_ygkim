0270f1d9fc44b037023f71177fe93cb187feccb1
영건 김 <young3702@naver.com>
2024-02-13 23:23:00 +0900

    20240213_WEBAPI(PUT,POST,DELETE)_updated, 20240213_WEBAPI(EmployeeService)_added

--- WebAPIDemo/WebAPIDemo/Controllers/ValuesController.cs
+++ WebAPIDemo/WebAPIDemo/Controllers/ValuesController.cs
@@ -7,33 +7,40 @@ using System.Web.Http;
 
 namespace WebAPIDemo.Controllers
 {
-    /*public class ValuesController : ApiController
+    public class ValuesController : ApiController
     {
+        static List<string> strings = new List<string>()
+        {
+            "value0", "value1", "value2"
+        };
         // GET api/values
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return strings;
         }
 
         // GET api/values/5
         public string Get(int id)
         {
-            return "value";
+            return strings[id];
         }
 
         // POST api/values
         public void Post([FromBody] string value)
         {
+            strings.Add(value);
         }
 
         // PUT api/values/5
         public void Put(int id, [FromBody] string value)
         {
+            strings[id] = value;
         }
 
         // DELETE api/values/5
         public void Delete(int id)
         {
+            strings.RemoveAt(id);
         }
     }
-*/}
+}
